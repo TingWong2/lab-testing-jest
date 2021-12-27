05cb76e2ff1399bf86e83d996e1fc36360c779fd
Ting Wong <ting_wong2@yahoo.fr>
2021-12-27 23:35:38 +0100

    updated spec files to run tests

--- src/cents-to-decimals.js
+++ src/cents-to-decimals.js
@@ -1,11 +1,11 @@
 function centsToDecimals(centValue) {
+  
   if (typeof centValue !== 'number' || isNaN(centValue)) {
     return undefined;
   }
 
-  let result = centValue / 100;
-
-  return result.toFixed(2) + '$';
+  const result = centValue / 100;
+  return `${result.toFixed(2)}$`;
 }
 
 // The following is required to make automated tests run. Please ignore it.
